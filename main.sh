6ed702504930cd79068a877053dc9810f1939f44
radix10 <admin@radix10.me>
2020-09-19 20:31:15 +0800

    tested

--- main.sh
+++ main.sh
@@ -5,27 +5,20 @@ set -x
 url=$1
 version=$2
 
-# change url
+# change url and log
 sed -i "/ADD/s| .* | ${url} |" Dockerfile
+echo "* ${url}[$version]" >> readme.adoc
 
-# log url and version
-echo "\* ${url}[$version]" >> readme.adoc
-
-# commit
+# commit and push
 git add Dockerfile readme.adoc
 git commit Dockerfile readme.adoc -m "Add ${version}"
 git tag "release-v${version}"
-
-# push
-git push
 git push origin "release-v${version}"
 
-# pull image
+# pull image and extract file
 until docker pull "registry.cn-beijing.aliyuncs.com/radix10/downloader:${version}"
 do
   echo 'Try pull image...'
   sleep 10
 done
-
-# extract file
-docker run --user $(id -u):$(id -g) --rm --detach --volume ./:/mnt "registry.cn-beijing.aliyuncs.com/radix10/downloader:${version}"
+docker run --user $(id -u):$(id -g) --rm --detach --volume $PWD:/mnt "registry.cn-beijing.aliyuncs.com/radix10/downloader:${version}"
