fd8f66e2e1b10fd3ac01322d5abfc8fd3ee905f0
radix10 <admin@radix10.me>
2020-09-27 22:45:33 +0800

    optimization

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cp /opt/download_file /mnt/download_file
--- main.sh
+++ main.sh
@@ -3,17 +3,18 @@
 set -x
 
 url=$1
-version=$2
+filename=$2
+version=$(git tag -l | sort -t. -k3 -n | awk -F 'v' 'END{print $2}' | awk -F '.' '{print $1 "." $2 "." $3 + 1}')
 
 # change url and log
 sed -i "/ADD/s| .* | ${url} |" Dockerfile
 echo "* ${url}[$version]" >> readme.adoc
 
 # commit and push
-git add Dockerfile readme.adoc
-git commit Dockerfile readme.adoc -m "Add ${version}"
-git tag "release-v${version}"
-git push origin "release-v${version}"
+proxychains git add Dockerfile readme.adoc
+proxychains git commit Dockerfile readme.adoc -m "Add ${version}"
+proxychains git tag "release-v${version}"
+proxychains git push origin "release-v${version}"
 
 # pull image and extract file
 until docker pull "registry.cn-beijing.aliyuncs.com/radix10/downloader:${version}"
@@ -21,4 +22,8 @@ do
   echo 'Try pull image...'
   sleep 30
 done
-docker run --user $(id -u):$(id -g) --rm --detach --volume $PWD:/mnt "registry.cn-beijing.aliyuncs.com/radix10/downloader:${version}"
+
+docker run --rm --detach --volume $PWD:/mnt "registry.cn-beijing.aliyuncs.com/radix10/downloader:${version}" \
+	sh -c "mv /opt/download_file /mnt/download_file && chown $(id -u):$(id -g) /mnt/download_file"
+
+mv download_file $filename
